fe575f25c8807cc968820ebc8f9ccac34774a93b
Alexey Astapov <astapaa@mail.ru>
2024-04-30 12:08:45 +0300

    project init

---
+++ main/java/org/javaacademy/bitcoin_to_rub/BitcoinToRubApplication.java
@@ -0,0 +1,13 @@
+package org.javaacademy.bitcoin_to_rub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitcoinToRubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitcoinToRubApplication.class, args);
+	}
+
+}
---
+++ test/java/org/javaacademy/bitcoin_to_rub/BitcoinToRubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaacademy.bitcoin_to_rub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitcoinToRubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
